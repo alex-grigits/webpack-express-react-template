ea1b9b7103f3c9c47de0e643a77fa1e206ae8412
Oleksii Tsyliuryk <Oleksii.Tsyliuryk@symphony-solutions.com>
2024-08-19 19:11:38 +0300

    fixed webpack configs and added some plugins

--- client/Card.jsx
+++ client/Card.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import SVG from './icon.svg';
-import './card.scss';
+import * as styles from './card.module.scss';
 
 export const Card = () => {
+  // console.log({ styles });
   return (
     <div>
       <SVG width="100" height="100" viewBox="0 0 100 100" />
       <h3>Card title example</h3>
       <p>Card description</p>
-      <button className="cardButton">Submit</button>
+      <button className={styles['primary-button']}>Submit12</button>
     </div>
   );
 };
